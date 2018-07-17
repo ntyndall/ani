915bfd5263786a756014227d0cb599ec59065626
Niall Tyndall <ntyndall@gmail.com>
2018-07-17 18:07:27 +0100

    Calculate centroid data from long/lat values

---
+++ aniR/R/calculate_centroid.R
@@ -0,0 +1,23 @@
+#' @title Calculate Centroid
+#'
+#' @description Calculates the centroid from a vector of
+#'  longitdues and latitudes and updates the appropriate
+#'  redis key.
+#'
+#' @export
+
+
+calculate_centroid(dbr, rKey, geoms) {
+
+  # Calculate centroid from geometries
+  result <- geoms %>%
+    matrix(ncol = 2) %>%
+    geosphere::centroid() %>%
+    as.double
+
+  # Update the redis key with centroid results
+  rKey %>% dbr$HMSET(
+    field = c('CENT_LONG', "CENT_LAT"),
+    value = result
+  )
+}
