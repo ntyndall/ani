ac3fc54912274b9e746d8eef929d2f1a0a6cae7e
Niall Tyndall <ntyndall@gmail.com>
2018-07-18 17:08:04 +0100

    Start to shape the API, write some docs for swagger

--- aniR/demo/api.R
+++ aniR/demo/api.R
@@ -1,11 +1,19 @@
+#* @apiTitle Analytics - Northern Ireland
+#*
+#* @apiDescription Manage and display relevant data based on tourism
+#*   finance, etc.
+#*
+#* @apiTag gardens Responsible for returning information based on
+#*   the locations of historic parks and gardens.
+
 library(magrittr)
 library(purrr)
 
 API_LINK <- "api.postcodes.io/postcodes/"
 
-#' Log some information about the incoming request
-#'
-#' @filter dbconnection
+#* Log some information about the incoming request
+#*
+#* @filter dbconnection
 
 
 function(req) {
@@ -24,11 +32,11 @@ function(req) {
   plumber::forward()
 }
 
-#' Echo the parameter that was sent in
-#'
-#' @param req The incoming request details.
-#'
-#' @get /
+#* Echo the parameter that was sent in
+#*
+#* @param req The incoming request details.
+#*
+#* @get /
 
 
 function(req) {
@@ -42,14 +50,26 @@ function(req) {
   allKeys %>% table %>% as.list
 }
 
-#' Echo the parameter that was sent in
-#'
-#' @param msg The message to echo back.
-#'
-#' @get /gardens
+#* Return historic parks and gardens, if _postcode_ is supplied
+#* then return the closest results
+#*
+#* @param postcode:character An NI postcode
+#* @param returnItems:int The number of results the API should return
+#*
+#* @get /gardens
+#*
+#* @response 200 Success
+#* @response 400 Invalid input provided
+#*
+#* @serializer unboxedJSON
+#*
+#* @tag gardens
 
 
-function(req, res, postcode = NULL, returnVals = NULL) {
+function(req, res, postcode = "", returnItems = 1) {
+
+  # Make sure values are integers
+  returnItems %<>% as.integer
 
   # Rename database connection
   dbr <- req$dbr
@@ -60,27 +80,37 @@ function(req, res, postcode = NULL, returnVals = NULL) {
     dbr$KEYS() %>%
     purrr::flatten_chr()
 
-  # Set returnVals
-  returnVals <- if (returnVals %>% is.null) {
+  # Set returnItems
+  returnItems <- if (returnItems == 0) {
     allKeys %>% length
   } else {
-    returnVals %>% min(allKeys %>% length)
+    returnItems %>% min(allKeys %>% length)
   }
 
   # Sort by the ID
   allKeys %<>% order_keys()
 
   # If postcode exists then search for it
-  if (postcode %>% is.null %>% `!`()) {
+  if (postcode %>% `!=`("")) {
+
+    # Query using the supplied postcode
     results <- API_LINK %>%
       paste0(postcode) %>%
       httr::GET()
+
+    # If postcode is successful then look up database
     if (results$status_code == 200) {
+
+      # Convert the results to a data frame
       my.data <- results$content %>%
         rawToChar() %>%
         jsonlite::fromJSON()
-      currentLoc <- c(my.data$result$longitude, my.data$result$latitude) %>% as.double
 
+      # Create a vector of the current location via long + lat
+      currentLoc <- c(my.data$result$longitude, my.data$result$latitude) %>%
+        as.double
+
+      # Collect ALL distances!
       allDists <- c()
       for (i in 1:(allKeys %>% length)) {
         newLoc <- allKeys[i] %>%
@@ -103,11 +133,11 @@ function(req, res, postcode = NULL, returnVals = NULL) {
       allKeys %<>% `[`(
         allDists %>%
           sort.int(
-            decreasing = TRUE,
+            decreasing = FALSE,
             index.return = TRUE
           ) %>%
           `$`('ix') %>%
-          head(returnVals)
+          head(returnItems)
       )
 
       # Get all
@@ -117,10 +147,18 @@ function(req, res, postcode = NULL, returnVals = NULL) {
   }
 
   # Return table list
-  if (postcode %>% is.null) {
-    allKeys %>% table %>% as.list
+  if (postcode %>% `==`("")) {
+    bigList <- allKeys %>% table %>% as.list
   } else {
-    allKeys %>% table %>% as.list
+    bigList <- list()
+    for (j in 1:returnItems) {
+      res <- allKeys[j] %>% dbr$HGETALL() %>% purrr::flatten_chr()
+      newres <- res[c(FALSE, TRUE)] %>% as.list
+      names(newres) <- res[c(TRUE, FALSE)]
+      bigList %<>% c(newres %>% list)
+    }
   }
-}
 
+  # Return bigList
+  bigList
+}
