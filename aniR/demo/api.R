350a2085754980d1b82a57da07f3eac47e0f51e5
Niall Tyndall <ntyndall@gmail.com>
2018-07-19 11:52:34 +0100

    Start to update the logic for returning appropriate maps back from the API

--- aniR/demo/api.R
+++ aniR/demo/api.R
@@ -154,105 +154,114 @@ function(req, postcode = "") {
   # Rename db connection from incoming request
   dbr <- req$dbr
 
-  # Calculate center from postcode
-  results <- API_LINK %>%
-    paste0(postcode) %>%
-    httr::GET()
-
-  # Convert the results to a data frame
-  my.data <- results$content %>%
-    rawToChar() %>%
-    jsonlite::fromJSON()
-
-  # Create a vector of the current location via long + lat
-  currentCenter <- c(my.data$result$longitude, my.data$result$latitude) %>%
-    as.double
-
-  # Create a flattened numeric vector
-  as_vec <- function(x) x %>% purrr::flatten_chr() %>% as.numeric
-
-  # Just pick these at random for testing
-  vals <- c(73, 69, 72, 77, 66)
-
-  # Collect all the data (still need to account for multiple polygons)
-  all.data <- list()
-  for (i in 1:5) {
-    test.data <- data.frame(
-      long = dbr$LRANGE(
-        key = paste0("park+garden/long/", vals[i]),
-        start = 0,
-        stop = -1
-      ) %>%
-        as_vec(),
-      lat = dbr$LRANGE(
-        key = paste0("park+garden/lat/", vals[i]),
-        start = 0,
-        stop = -1
-      ) %>%
-        as_vec(),
-      stringsAsFactors = FALSE
-    )
-
-    # Always check the extremeties..
-    if (test.data$long %>% min %>% `<`(longMinMax[1]) || i == 1) longMinMax[1] <- test.data$long %>% min
-    if (test.data$long %>% max %>% `>`(longMinMax[2]) || i == 1) longMinMax[2] <- test.data$long %>% max
-    if (test.data$lat %>% min %>% `<`(latMinMax[1]) || i == 1) latMinMax[1] <- test.data$lat %>% min
-    if (test.data$lat %>% max %>% `>`(latMinMax[2]) || i == 1) latMinMax[2] <- test.data$lat %>% max
-
-    # Save all the data in a list for later!
-    all.data %<>% c(test.data %>% list)
-  }
-
-  # Add the center into min and max..
-  if (currentCenter[1] < longMinMax[1]) longMinMax[1] <- currentCenter[1]
-  if (currentCenter[1] > longMinMax[2]) longMinMax[2] <- currentCenter[1]
-  if (currentCenter[2] < latMinMax[1]) latMinMax[1] <- currentCenter[2]
-  if (currentCenter[2] > latMinMax[2]) latMinMax[2] <- currentCenter[2]
-
-  # Spread out just a little (to be on the safe side)
-  longMinMax[1] %<>% `-`(0.01)
-  longMinMax[2] %<>% `+`(0.01)
-  latMinMax[1] %<>% `-`(0.01)
-  latMinMax[2] %<>% `+`(0.01)
-
-  # Calculate zoom factor here
-  zoomVal <- list(
-    long = longMinMax,
-    lat = latMinMax
-  ) %>%
-    aniR::calculate_zoom()
-
-  # Calculate new center position
-  newCenter <- c(
-    longMinMax[2] %>% `-`(longMinMax[1]) %>% `/`(2) %>% `+`(longMinMax[1]),
-    latMinMax[2] %>% `-`(latMinMax[1]) %>% `/`(2) %>% `+`(latMinMax[1])
-  )
-
-  # Define the map from google maps
-  g <- ggmap::get_googlemap(
-    center = newCenter,
-    maptype = 'roadmap',
-    zoom = zoomVal,
-    scale = 2
-  ) %>%
-    ggmap::ggmap()
-
-  # Add in all the polygons
-  for (i in 1:5) {
-    single.data <- all.data[[i]]
-    g %<>% `+`(
-      ggplot2::geom_polygon(
-        data = ggplot2::fortify(single.data),
-        mapping = ggplot2::aes(long, lat),
-        fill = "orange",
-        colour = "red",
-        alpha = 0.2
+  # Calculate longitutde and latitude from postcode
+  if (postcode %>% `!=`("")) {
+    postC <- API_LINK %>%
+      aniR::get_postcode(
+        postcode = postcode
       )
-    )
-  }
 
-  # Need to add a theme for plotting here
-  # g %<>% aniR::google_theme()
+    if (postC$status == 200) {
+      currentCenter <- postC$location
+
+      # Create a flattened numeric vector
+      as_vec <- function(x) x %>% purrr::flatten_chr() %>% as.numeric
+
+      # Just pick these at random for testing
+      vals <- c(73, 69, 72, 77, 66)
+
+      # Collect all the data (still need to account for multiple polygons)
+      all.data <- list()
+      for (i in 1:5) {
+        test.data <- data.frame(
+          long = dbr$LRANGE(
+            key = paste0("park+garden/long/", vals[i]),
+            start = 0,
+            stop = -1
+          ) %>%
+            as_vec(),
+          lat = dbr$LRANGE(
+            key = paste0("park+garden/lat/", vals[i]),
+            start = 0,
+            stop = -1
+          ) %>%
+            as_vec(),
+          stringsAsFactors = FALSE
+        )
+
+        # Always check the extremeties..
+        if (test.data$long %>% min %>% `<`(longMinMax[1]) || i == 1) longMinMax[1] <- test.data$long %>% min
+        if (test.data$long %>% max %>% `>`(longMinMax[2]) || i == 1) longMinMax[2] <- test.data$long %>% max
+        if (test.data$lat %>% min %>% `<`(latMinMax[1]) || i == 1) latMinMax[1] <- test.data$lat %>% min
+        if (test.data$lat %>% max %>% `>`(latMinMax[2]) || i == 1) latMinMax[2] <- test.data$lat %>% max
+
+        # Save all the data in a list for later!
+        all.data %<>% c(test.data %>% list)
+      }
+
+      # Add the center into min and max..
+      if (currentCenter[1] < longMinMax[1]) longMinMax[1] <- currentCenter[1]
+      if (currentCenter[1] > longMinMax[2]) longMinMax[2] <- currentCenter[1]
+      if (currentCenter[2] < latMinMax[1]) latMinMax[1] <- currentCenter[2]
+      if (currentCenter[2] > latMinMax[2]) latMinMax[2] <- currentCenter[2]
+
+      # Spread out just a little (to be on the safe side)
+      longMinMax[1] %<>% `-`(0.01)
+      longMinMax[2] %<>% `+`(0.01)
+      latMinMax[1] %<>% `-`(0.01)
+      latMinMax[2] %<>% `+`(0.01)
+
+      # Calculate zoom factor here
+      zoomVal <- list(
+        long = longMinMax,
+        lat = latMinMax
+      ) %>%
+        aniR::calculate_zoom()
+
+      # Calculate new center position
+      newCenter <- c(
+        longMinMax[2] %>% `-`(longMinMax[1]) %>% `/`(2) %>% `+`(longMinMax[1]),
+        latMinMax[2] %>% `-`(latMinMax[1]) %>% `/`(2) %>% `+`(latMinMax[1])
+      )
+
+      # Define the map from google maps
+      g <- ggmap::get_googlemap(
+        center = newCenter,
+        maptype = 'roadmap',
+        zoom = zoomVal,
+        scale = 2
+      ) %>%
+        ggmap::ggmap()
+
+      # Add in all the polygons
+      for (i in 1:5) {
+        single.data <- all.data[[i]]
+        g %<>% `+`(
+          ggplot2::geom_polygon(
+            data = ggplot2::fortify(single.data),
+            mapping = ggplot2::aes(long, lat),
+            fill = "orange",
+            colour = "red",
+            alpha = 0.2
+          )
+        )
+      }
+
+      # Need to add a theme for plotting here
+      # g %<>% aniR::google_theme()
+
+    } else {
+      res$status <- postC$status
+    }
+  } else {
+    g <- ggmap::get_googlemap(
+      center = c(-6.7, 54.7),
+      maptype = 'roadmap',
+      zoom = 8,
+      scale = 2
+    ) %>%
+      ggmap::ggmap()
+  }
 
   # Return the plot back from the endpoint
   plot(g)
